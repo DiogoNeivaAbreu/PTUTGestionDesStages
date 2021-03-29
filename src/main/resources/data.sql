94493f31d389f1711169fc2e2eecf53eff142b2e
Diogo <Asus@10.157.4.149>
2021-03-29 17:05:46 +0200

    Nouvelle page pour mon profil

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,6 @@
 -- Les données qui seront initialisées automatiquement quand on lance l'application
 -- cf. application.properties
+/*
 INSERT INTO Stage(DTYPE, id, titre, description, date_debut, date_fin) VALUES 
     ('OffreStage', 1, 'Informatique', 'Stage de 3A', TO_DATE('2021-05-01', 'YYYY-MM-DD'), TO_DATE('2021-31-07', 'YYYY-MM-DD')),
     ('OffreStage', 2, 'Informatique', 'Stage de 3A', TO_DATE('2021-05-01', 'YYYY-MM-DD'), TO_DATE('2021-31-07', 'YYYY-MM-DD'));
@@ -11,3 +12,4 @@ INSERT INTO Entreprise(id, nom, adresse, telephone, email, secteur) VALUES
 INSERT INTO Personne(DTYPE, id, nom, prenom, adresse, telephone, email, poste, matiere_enseignee, annee_etude) VALUES
     ('Etudiant', 1, 'Dubois', 'Pierre', 'France', '0671827384', null, null, null, TO_DATE('2021', 'YYYY')),
     ('MaitreStageEcole', 2, 'Petit', 'Jean', 'France', '0694857844', null, null, 'Informatique', null));
+*/
