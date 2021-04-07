8c485ea61515a7ccf0e27f8d6ba8f68bab0ee0da
Diogo <Asus@DESKTOP-GV5ISNM.home>
2021-04-07 15:42:19 +0200

    Mise en place des logins

--- src/main/java/gestionStages/controller/LoginAndRegistrationController.java
+++ src/main/java/gestionStages/controller/LoginAndRegistrationController.java
@@ -27,26 +27,26 @@ public class LoginAndRegistrationController {
         this.userValidator = userValidator;
     }
 
-    @GetMapping("/registration")
+    @GetMapping("/creerUnCompte")
     public String registration(Model model) {
         model.addAttribute("userForm", new Utilisateur());
 
-        return "registration";
+        return "creerUnCompte";
     }
 
-    @PostMapping("/registration")
+    @PostMapping("/creerUnCompte")
     public String registration(@Valid @ModelAttribute("userForm") Utilisateur userForm, BindingResult bindingResult) {
         userValidator.validate(userForm, bindingResult);
 
         if (bindingResult.hasErrors()) {
-            return "registration";
+            return "creerUnCompte";
         }
 
         userService.save(userForm);
 
         securityService.autoLogin(userForm.getUsername(), userForm.getPasswordConfirm());
 
-        return "redirect:/welcome";
+        return "redirect:/index";
     }
 
     @GetMapping("/login")
@@ -59,4 +59,9 @@ public class LoginAndRegistrationController {
 
         return "login";
     }
+
+//    @GetMapping({"/", "/welcome"})
+//    public String welcome(Model model) {
+//        return "welcome";
+//    }
 }
